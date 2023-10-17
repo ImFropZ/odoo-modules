44cf86b13d523b4ab387133a66e6517b9ed9cc87
FropZ <limtangmeng100@gmail.com>
2023-10-17 14:39:16 +0700

    Fixed the json parse error

--- youtube_api_anakut/controllers/youtube_api.py
+++ youtube_api_anakut/controllers/youtube_api.py
@@ -8,19 +8,13 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class YoutubeAPI(http.Controller):
-    @http.route(['/api/youtube/<channel_id>/videos', '/api/youtube/<channel_id>/videos/<next_page_token>'], type='http', auth='public', methods=['GET'], csrf=False)
+    @http.route(['/api/youtube/<channel_id>/videos'], type='http', auth='public', methods=['GET'], csrf=False)
     def query_videos(self, **payload):
         try:
             API_KEY = 'AIzaSyDTFpWxQjN8bS5a1oiBeV5OmcvBHB4geaU'
             CHANNEL_ID = payload['channel_id']
             max_results = payload.get('max_results', 10)
-
-            try:
-                NEXT_PAGE_TOKEN = payload['next_page_token']
-            except:
-                NEXT_PAGE_TOKEN = None
-
-            _logger.info(f"NEXT_PAGE_TOKEN: {NEXT_PAGE_TOKEN}")
+            page_token = payload.get('page_token', None)
 
             end_point = "https://www.googleapis.com/youtube/v3/search"
 
@@ -40,8 +34,8 @@ class YoutubeAPI(http.Controller):
             end_point += f"&key={API_KEY}"
 
             # Add next page token
-            if NEXT_PAGE_TOKEN:
-                end_point += f"&pageToken={NEXT_PAGE_TOKEN}"
+            if page_token:
+                end_point += f"&pageToken={page_token}"
 
             response = requests.get(end_point)
             return json.dumps(response.json())
--- youtube_api_anakut/static/src/snippets/s_youtube_videos/000.js
+++ youtube_api_anakut/static/src/snippets/s_youtube_videos/000.js
@@ -7,11 +7,26 @@ odoo.define("youtube_api_anakut.s_youtube_videos", function(require) {
   const SYoutubeVideos = PublicWidget.Widget.extend({
     selector: "#dynamic_snippet_youtube_videos",
 
+    read_events: {
+      "click .youtube_load_more_btn": "_loadMoreVideos",
+    },
+
     willStart: function() {
       // biome-ignore lint/style/noArguments: <explanation>
       return this._super.apply(this, arguments).then(() => {
-        this.max_results = this.$el.data("max_results");
+        this.container = this.$el.find(".container-youtube-videos");
+        this.loadMoreBtn = this.$el.find(".youtube_load_more_btn");
+
+        this.pageIndex = 0;
+
+        this.max_results = this.$el.data("number_of_videos");
+        if (this.max_results === undefined) {
+          this.max_results = 10;
+        }
         this.channel_id = this.$el.data("channel_id");
+        if (this.channel_id === undefined) {
+          return;
+        }
         this.videos = [];
         return Promise.all([this._getVideos()]);
       });
@@ -20,23 +35,51 @@ odoo.define("youtube_api_anakut.s_youtube_videos", function(require) {
     start: function() {
       // Get attribute from this el
       this.channel_link = this.$el.data("youtube_channel_link");
-
-      this.$el.closest(".subscribe_btn").attr("href", this.channel_link);
+      this.$el.find(".subscribe_btn").attr("href", this.channel_link);
 
       this._render();
     },
 
     _getVideos: async function() {
-      return await fetch(`/api/youtube/${this.channel_id}/videos` + this.max_results ? `?max_results=${this.max_results}` : "").then(res => res.json()).then(data => {
+      return await fetch(`/api/youtube/${this.channel_id}/videos?max_results=${this.max_results}`).then(res => res.json()).then(data => {
         this.nextPageToken = data.nextPageToken;
+        if (this.nextPageToken === undefined || this.nextPageToken === null || this.nextPageToken === "") {
+          this.loadMoreBtn.attr("disabled", true);
+        }
         this.videos = data.items;
+      }).catch(err => {
+        console.log(err);
       })
     },
 
     _render: function() {
-      $("#dynamic_snippet_youtube_videos .container-youtube-videos").html(QWeb.render("youtube_api_anakut.YoutubeIframes", {
-        videos: this.videos
+      if (!this.videos) {
+        if (!this.channel_id) {
+          this.container.html("<p>Channel ID not found</p>");
+        }
+        return;
+      }
+      if (this.pageIndex === 0) {
+        this.container.html("");
+      }
+      console.log(this.videos);
+      this.container.append(QWeb.render("youtube_api_anakut.YoutubeIframes", {
+        videos: this.videos.slice(this.pageIndex * this.max_results)
       }));
+    },
+
+    _loadMoreVideos: async function() {
+      return await fetch(`/api/youtube/${this.channel_id}/videos?max_results=${this.max_results}&page_token=${this.nextPageToken}`).then(res => res.json()).then(data => {
+        this.nextPageToken = data.nextPageToken;
+        if (this.nextPageToken === undefined || this.nextPageToken === null || this.nextPageToken === "") {
+          this.loadMoreBtn.attr("disabled", true);
+        }
+        this.videos = this.videos.concat(data.items);
+        this.pageIndex++;
+        this._render();
+      }).catch(err => {
+        console.log(err);
+      })
     }
   });
 
